25f76f59ef197c80f11e9a001b2bb7d3ef517d7f
Kacper-Wierzba <kacper.wierzbaa@wp.pl>
2025-05-21 19:53:37 +0200

    added skeleton of interface

---
+++ Classes/Interface.py
@@ -0,0 +1,29 @@
+﻿class Interface:
+    pass
+    #dodaj ucznia
+        #wpisz imie
+        #wpisz nazwisko
+        #wpisz pesel
+        #wpisz klasę lub - jeżęli nie chcesz teraz przypisywać do klasy
+    #indywidualny uczeń
+        #wybierz klasę
+            #wybierz ucznia
+                #pokaż listę ocen i obecności
+                #wybierz czy chcesz usunać czy edytować
+                    #jeżeli usunąć to usuń jeżeli edytować to proś o nowe dane
+                #można wybrać pokazanie średniej albo czy jest zagrożony
+    #pokaż zagrożenia
+        #przejdź po uczniach i pokaż czy są zagrożeni
+    #sprawdzenie list obecności albo ocen
+        #wybierz klasę
+            #wpisz przedmiot
+                #wybierz obecność/ocena pewnie nie powinno się dać sprawdzić 1 dnia tego smaego przedmiotu
+                    #wyświetl listę uczniów 1 po drugim
+                        #przy każdym wpisz czy jest czy nie
+                    #pokaż listę wszystkich uczniów
+                        #wybierz który
+                            #wpisz ocenę
+                    #wyjdź tylko jak kliknie się guzik
+
+
+
