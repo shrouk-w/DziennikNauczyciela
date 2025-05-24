3c05d6109f7b57d4545e69866d87f5164a30fe29
Kacper-Wierzba <kacper.wierzbaa@wp.pl>
2025-05-24 13:26:36 +0200

    additional type conformation added

--- Classes/Grade.py
+++ Classes/Grade.py
@@ -11,6 +11,8 @@ class Grade:
     def __init__(self, grade, desc, nameOfClass, type, date=date.today()):
         if not isinstance(grade, (int, float)):
             raise InvalidGradeType("Ocena musi być liczbą")
+        if grade < 1 or grade > 6:
+            raise InvalidGradeType("oceny mieszczą się w przedziale 1-6")
         if not isinstance(desc, str):
             raise InvalidDescriptionType("Opis musi być w postaci tekstu")
         if not isinstance(nameOfClass, str):
