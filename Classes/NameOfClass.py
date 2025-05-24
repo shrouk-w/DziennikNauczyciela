d42b2ff6e7336386879c1f4fa8bc99bffce06772
Kacper-Wierzba <kacper.wierzbaa@wp.pl>
2025-05-24 20:47:26 +0200

    added hinters

--- Classes/NameOfClass.py
+++ Classes/NameOfClass.py
@@ -5,7 +5,7 @@ class NameOfClass:
     className = []
 
     @classmethod
-    def add_class(cls, classname: str):
+    def add_class(cls, classname: str) -> None:
         if not isinstance(classname, str):
             raise InvalidClassNameType("Nazwa klasy musi być w formie tekstu")
         if classname in cls.className:
@@ -13,7 +13,7 @@ class NameOfClass:
         cls.className.append(classname)
 
     @classmethod
-    def del_class(cls, classname: str):
+    def del_class(cls, classname: str) -> None:
         if not isinstance(classname, str):
             raise InvalidClassNameType("Nazwa klasy musi być w formie tekstu")
         if classname not in cls.className:
@@ -21,9 +21,9 @@ class NameOfClass:
         cls.className.remove(classname)
 
     @classmethod
-    def show_class_names(cls):
+    def show_class_names(cls) -> None:
         print(cls.className)
 
     @classmethod
-    def isNameOfClass(cls, classname: str):
+    def isNameOfClass(cls, classname: str) -> bool:
         return classname in cls.className
